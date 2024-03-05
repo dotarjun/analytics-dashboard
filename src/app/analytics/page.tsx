05fc081bc35860ca14f62db209a0c75a6c13aa6c
dotarjun <71163609+dotarjun@users.noreply.github.com>
2024-03-05 18:43:16 +0530

    feat: weekly avg visitors + current day visitors
    Signed-off-by: dotarjun <71163609+dotarjun@users.noreply.github.com>

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,14 +1,44 @@
+import AnalyticsDashboard from "@/components/AnalyticsDashboard";
+import { getDate } from "@/utils";
 import { analytics } from "@/utils/analytics";
 
 const Page = async () => {
-  const pageview = await analytics.retrieveAll("pageview", 2);
+  const TRACKING_DAYS = 7;
 
-  return;
-  <div className="min-h-screen w-full py-12 flex justify-center items-center">
-    <div className="relative w-full max-w-6xl mx-auto text-white">
-      <AnalyticsDashboard />>
+  const pageviews = await analytics.retrieveAll("pageview", TRACKING_DAYS);
+
+  const totalPageViews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1);
+
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => {
+          return acc + Object.values(curr)[0]!;
+        }, 0)
+      );
+    }, 0);
+
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <AnalyticsDashboard
+          avgVisitorsPerDay={avgVisitorsPerDay}
+          amtVisitorsToday={amtVisitorsToday}
+        />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Page;
